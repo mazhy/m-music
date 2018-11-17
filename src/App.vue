84182c9cbdc40c743d6d7d6b371ed536178703c8
mazhaoyuan <mazy0115@163.com>
2018-11-17 10:30:27 +0800

    项目初始化

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <div id="app">
+    hello world
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App'
+}
+</script>
+
+<style scoped lang="stylus" rel="stylesheet/stylus">
+@import 'common/stylus/variable.styl';
+#app {
+  background: $color-background
+  color: $color-theme-d
+}
+</style>
